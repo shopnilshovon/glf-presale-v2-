6458c1f4d0e1762b024118bdd326a86884d8fda5
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-03 23:53:05 +0600

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,16 @@
+export const PRESALE_CONTRACT_ADDRESS = "0xF487630170DDF50ad4767A90248aC183d5e29Aee";
+export const USDT_TOKEN_ADDRESS = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+export const GLF_TOKEN_ADDRESS = "0xB4b628464F499118340A8Ddf805EF9E18B624310";
 
+export const POLYGON_CHAIN_ID = "0x89"; // 137 in hex
+export const POLYGON_CHAIN_PARAMS = {
+  chainId: POLYGON_CHAIN_ID,
+  chainName: "Polygon Mainnet",
+  nativeCurrency: {
+    name: "MATIC",
+    symbol: "MATIC",
+    decimals: 18,
+  },
+  rpcUrls: ["https://polygon-rpc.com"],
+  blockExplorerUrls: ["https://polygonscan.com/"],
+};
