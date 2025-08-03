600b6f868c71696d7ab7946a6a76d03d3b727a81
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-03 23:55:55 +0600

    Update useContract.js

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,1 +1,30 @@
+import { useEffect, useState } from "react";
+import { Contract, BrowserProvider } from "ethers";
+import presaleAbi from "../abis/PresaleABI.json";
+import { PRESALE_CONTRACT_ADDRESS } from "../utils/constants";
 
+const useContract = (account) => {
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      if (window.ethereum && account) {
+        const browserProvider = new BrowserProvider(window.ethereum);
+        const signer = await browserProvider.getSigner();
+        const presaleContract = new Contract(PRESALE_CONTRACT_ADDRESS, presaleAbi, signer);
+        setContract(presaleContract);
+        setProvider(browserProvider);
+      } else {
+        setContract(null);
+        setProvider(null);
+      }
+    };
+
+    init();
+  }, [account]);
+
+  return { contract, provider };
+};
+
+export default useContract;
