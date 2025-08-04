a815020733eaadc354db62890feb30d37a3e87e5
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-04 14:25:02 +0600

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-// Presale contract deployed address
 export const PRESALE_CONTRACT_ADDRESS = "0xF487630170DDF50ad4767A90248aC183d5e29Aee";
-
-// Polygon USDT token address (6 decimals)
 export const USDT_TOKEN_ADDRESS = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
