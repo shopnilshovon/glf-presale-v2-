5dfe371e8914771ddd60c1d84ee4370cde36d856
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-04 14:28:30 +0600

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,9 @@
-import { Toaster } from "react-hot-toast";
+export default function Notification({ type, message }) {
+  const color = type === "success" ? "bg-green-600" : "bg-red-600";
 
-const Notification = () => {
-  return <Toaster position="top-center" />;
-};
-
-export default Notification;
+  return (
+    <div className={`${color} text-white px-4 py-2 rounded mb-4`}>
+      {message}
+    </div>
+  );
+}
