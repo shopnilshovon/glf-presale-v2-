11fa51eabb8ebea2900c663a8c8681a969fddef3
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-04 12:20:56 +0600

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,1 +1,13 @@
+export default function Notification({ type, message }) {
+  const baseStyle = "text-white px-4 py-3 rounded shadow text-center";
+  const styles = {
+    success: "bg-green-600",
+    error: "bg-red-600",
+  };
 
+  return (
+    <div className={`${baseStyle} ${styles[type] || "bg-gray-600"}`}>
+      {message}
+    </div>
+  );
+}
